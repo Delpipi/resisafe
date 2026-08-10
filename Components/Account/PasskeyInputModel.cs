1a2983f117e7aef704a4977b8ed3d56283229375
Delpipi <alexandrepaulkouame@gmail.com>
2026-08-10 23:04:12 +0000

    completed auth operation

---
+++ Components/Account/PasskeyInputModel.cs
@@ -0,0 +1,7 @@
+namespace resisafe.Components.Account;
+
+public class PasskeyInputModel
+{
+    public string? CredentialJson { get; set; }
+    public string? Error { get; set; }
+}
---
+++ Components/Account/PasskeyOperation.cs
@@ -0,0 +1,7 @@
+namespace resisafe.Components.Account;
+
+public enum PasskeyOperation
+{
+    Create = 0,
+    Request = 1,
+}
